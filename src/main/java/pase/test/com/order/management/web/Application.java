4522fa77ee0aa5653c228c68682d0caec56a3b9a
ErickReyesMontalvo <eriick732@gmail.com>
2025-07-15 11:32:39 -0600

    [PC-00] chore create docker compose and add vars

--- src/main/java/pase/test/com/order/management/web/Application.java
+++ src/main/java/pase/test/com/order/management/web/Application.java
@@ -1,13 +1,21 @@
 package pase.test.com.order.management.web;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    @PostConstruct
+    public void init() {
+        // Setting Spring Boot SetTimeZone
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
