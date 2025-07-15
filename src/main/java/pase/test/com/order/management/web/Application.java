d8dcace573748af58c917bdbb08da992bbb4d3c9
ErickReyesMontalvo <areyes@neopartners.mx>
2025-07-14 22:15:44 -0600

    [PC-00] chore create project structure order management web

---
+++ src/main/java/pase/test/com/order/management/web/Application.java
@@ -0,0 +1,13 @@
+package pase.test.com.order.management.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/pase/test/com/order/management/web/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pase.test.com.order.management.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
